ef0aadca6d3f2b84a107a281ea68eb97e3750275
jvitorn <joaoeuno2011@gmail.com>
2020-03-30 21:26:50 -0300

    adicionando req no controller usuario

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,3 +1,5 @@
+//geração de id 
+const generateUniqueId = require('../utils/generateUniqueId');
 //conexao com o banco de dados
 const connection = require('../database/connection');
 
@@ -5,7 +7,27 @@ module.exports = {
     //listagem da tabela endereco
     async index(req,res){
         const usuarios = await connection('usuario').select('*');
-
+        
         return res.json(usuarios);
+    },
+    async create(req,res){
+        const { nome,email,senha,celular='(00)000000000',nivel='user',imagem='semimagem' } = req.body;
+        //capturando id do endereco passado pelo cabecalho da aplicação
+        const endereco_id = req.headers.adress;
+        //criando ids aleatorios atraves do crýpto 
+        const id = generateUniqueId();
+        
+        await connection('usuario').insert({
+            id,
+            nome,
+            email,
+            senha,
+            celular,
+            nivel,
+            imagem,
+            endereco_id
+        }); 
+        //retornando id gerado pelo cryto no banco de dados 
+        return res.json({ id });
     }
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const EnderecoController = require('../controllers/EnderecoController');
+const UsuarioController = require('../controllers/UsuarioController');
 const routes = express.Router();
 //criar um novo endereco
 routes.post('/endereco',EnderecoController.create);
 //listar um novo endereco 
 routes.get('/endereco',EnderecoController.index);
+//criar um novo endereco
+routes.post('/usuario',UsuarioController.create);
+//listar um novo endereco 
+routes.get('/usuario',UsuarioController.index);
 //-----------------------------------------------------------------------------------------------------------------------------------------------
 //quando entrar na rota padrao sera iniciado o 'index.html'
 routes.get('/',(req,res)=>{
